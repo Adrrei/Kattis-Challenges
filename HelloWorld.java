fb3e8c7d53ab6a58760403ea5dcc6b615bb6cdc5
Adrian Reithaug <ar+service@uru.no>
2017-05-31 22:33:10 +0200

    Solution for ID: hello - Difficulty: 1.2

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+Problem: https://open.kattis.com/problems/hello
+Author: Adrian Reithaug
+Submitted: January 29th, 2017
+Time: 0.08s / 5.00s
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
