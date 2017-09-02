239633487b767753ed9da8057a5e59f0467f1530
Adrian Reithaug <ar+service@uru.no>
2017-09-02 19:09:51 +0200

    Solution for ID: planina - Difficulty: 1.3

---
+++ Planina.cs
@@ -0,0 +1,21 @@
+﻿/*
+Problem: https://open.kattis.com/problems/planina
+Author: Adrian Reithaug
+Submitted: September 2nd, 2017
+Time: 0.02s / 1.00s
+*/
+
+using System;
+
+namespace Kattis
+{
+    class Planina
+    {
+        static void Main(string[] args)
+        {
+            int numIterations = int.Parse(Console.ReadLine());
+            int numPoints = (int)Math.Pow(2, numIterations) + 1;
+            Console.WriteLine(numPoints * numPoints);
+        }
+    }
+}
