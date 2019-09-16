b0bf122bb5d21828f6bb32bb6fbac0285e2e9ff3
Adrian Reithaug <ar+service@uru.no>
2019-09-16 19:41:49 +0200

    Solution for ID: numberfun - Difficulty: 1.3

---
+++ NumberFun.cs
@@ -0,0 +1,58 @@
+﻿/*
+Problem: https://open.kattis.com/problems/numberfun
+Author: Adrian Reithaug
+Submitted: September 16th, 2019
+Time: 0.06s / 1.00s
+*/
+
+using System;
+
+namespace Kattis
+{
+    public class Output
+    {
+        public const string Possible = "Possible";
+        public const string Impossible = "Impossible";
+    }
+
+    public class NumberFun
+    {
+        private static void Main(string[] args)
+        {
+            int numCases = int.Parse(Console.ReadLine());
+
+            while (numCases-- > 0)
+            {
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                int a = numbers[0];
+                int b = numbers[1];
+                int result = numbers[2];
+
+                bool canProduceResult = Calculate(a, b, result);
+                string verdict = canProduceResult ? Output.Possible : Output.Impossible;
+
+                Console.WriteLine(verdict);
+            }
+        }
+
+        private static bool Calculate(int a, int b, int result)
+        {
+            if (a + b == result)
+                return true;
+
+            if (a - b == result || b - a == result)
+                return true;
+
+            if (a * b == result)
+                return true;
+
+            if (a % b == 0 && a / b == result)
+                return true;
+
+            if (b % a == 0 && b / a == result)
+                return true;
+
+            return false;
+        }
+    }
+}
